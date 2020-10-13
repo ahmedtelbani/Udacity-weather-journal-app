9a7e59e29bd961dae771e9585bf61ba5f03b6090
Ahmed EL_Telbani <69460945+ahmedtelbani@users.noreply.github.com>
2020-10-13 21:21:52 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,5 +1,73 @@
 /* Global Variables */
 
+//const express = require("express");
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1 + '.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Personal API key for OpenWeathMap API
+let baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?zip=';
+let apiKey = "&APPID=3d5cf2cb541fc95393f7b6be55443e7a";
+
+// add EventListener to HTML DOM Element
+document.getElementById("generate").addEventListener("click", performAction);
+
+// function called by EventListener
+function performAction(e){
+    const newZip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+
+    getWeather(baseUrl, newZip, apiKey).then(function(data) {
+        console.log(data);
+        // add data to POST request
+        postData('/add', {date:d, temp:data.list[0].main.temp, content: feelings});
+        UpdateUI(); 
+    });
+
+};
+
+// Function to GET web API data
+const getWeather = async (baseUrl, zip, key) => {
+    const res = await fetch(baseUrl+zip+key);
+    try{
+        const data = await res.json();
+        return data;
+    }catch(error){
+        console.log("error", error);
+        // handele the error
+    }
+}
+
+const postData = async (url='', data={}) =>{
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'Post',
+        Credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    try{
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }catch(error){
+        console.log("error", error);
+    }
+}
+
+// Function to get Project Data
+const UpdateUI = async () =>{
+    const request = await fetch('/all');
+    try{
+        const allData = await response.json();
+        document.getElementById('date').innerHTML = `Data: ${allData[0].data}`;
+        document.getElementById('temp').innerHTML = `Temperature: ${allData[0].temp}`;
+        document.getElementById('content').innerHTML = `feel: ${allData[0].content}`;
+    }catch(error){
+        console.log("error", error);
+    }
+}
